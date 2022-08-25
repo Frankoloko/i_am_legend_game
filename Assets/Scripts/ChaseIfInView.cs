40dd0f76708b8928583646fb44f8e3f981a59fb0
Francois <francoiskrugermail@gmail.com>
2022-08-25 07:04:27 +0200

    Added the clock idea

--- Assets/Scripts/ChaseIfInView.cs
+++ Assets/Scripts/ChaseIfInView.cs
@@ -9,12 +9,18 @@ public class ChaseIfInView : MonoBehaviour
     [SerializeField] GameObject fieldOfViewObject;
     FieldOfView fieldOfViewScript;
 
-    public GameObject chaseObject;
+    public string chaseObjectString;
 
     private float viewDistance;
+    private GameObject chaseObject;
 
     void Start()
     {
+        // Instead of letting the user assign a game object, we just let the user supply a string
+        // and then we will find the game object. We do this because in prefab viewer, you can't reference
+        // things outside of the prefab (so you can't drag the Player object into this field)
+        chaseObject = GameObject.Find(chaseObjectString);
+
         fieldOfViewScript = fieldOfViewObject.GetComponent<FieldOfView>();
     }
 
--- Assets/Scripts/SyncTransform.cs
+++ Assets/Scripts/SyncTransform.cs
@@ -6,9 +6,36 @@ public class SyncTransform : MonoBehaviour
 {
     [SerializeField] GameObject syncToObject;
 
+    [SerializeField] bool positionX;
+    [SerializeField] bool positionY;
+    [SerializeField] bool positionZ;
+    [SerializeField] bool rotationX;
+    [SerializeField] bool rotationY;
+    [SerializeField] bool rotationZ;
+    [SerializeField] bool scaleX;
+    [SerializeField] bool scaleY;
+    [SerializeField] bool scaleZ;
+
     void Update()
     {
         // Each frame, sync this object's values to the syncToObject
-        this.transform.position = new Vector3(syncToObject.transform.position.x, syncToObject.transform.position.y, this.transform.position.z);
+
+        // result = (if statemtent) ? (if true) : (if false);
+
+        float positionXValue = positionX ? syncToObject.transform.position.x : this.transform.position.x;
+        float positionYValue = positionY ? syncToObject.transform.position.y : this.transform.position.y;
+        float positionZValue = positionZ ? syncToObject.transform.position.z : this.transform.position.z;
+
+        float scaleXValue = scaleX ? syncToObject.transform.localScale.x : this.transform.localScale.x;
+        float scaleYValue = scaleY ? syncToObject.transform.localScale.y : this.transform.localScale.y;
+        float scaleZValue = scaleZ ? syncToObject.transform.localScale.z : this.transform.localScale.z;
+
+        float rotationXValue = rotationX ? syncToObject.transform.rotation.x : this.transform.rotation.x;
+        float rotationYValue = rotationY ? syncToObject.transform.rotation.y : this.transform.rotation.y;
+        float rotationZValue = rotationZ ? syncToObject.transform.rotation.z : this.transform.rotation.z;
+
+        this.transform.position = new Vector3(positionXValue, positionYValue, positionZValue);
+        this.transform.eulerAngles = new Vector3(rotationXValue, rotationYValue, rotationZValue);
+        this.transform.localScale = new Vector3(scaleXValue, scaleYValue, scaleZValue);
     }
 }
